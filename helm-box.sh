e658a98619a3ab1b35fc2637ac4f8a0d5b16c2f2
Saggi Mizrahi <saggi@mizrahi.cc>
2020-04-05 17:26:30 +0300

    minor fixes

--- helm-box.sh
+++ helm-box.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ $# != 3 ]
 then
-    echo "usage: $0 <kube-config-path> <work-dir-path> <pgp-dir-path>"
+    echo "usage: $0 <kube-config-path> <work-dir-path> <gpg-dir-path>"
     exit 1
 fi
 
 kube_config_path=$(readlink -f "$1")
 work_dir_path=$(readlink -f "$2")
-pgp_dir_path=$(readlink -f "$3")
-podman run -it --rm -v "$kube_config_path:/root/.kube/config:Z" -v "$work_dir_path:/config:Z" -v "$pgp_dir_path:/root/.gnupg:Z" helm-box
+gpg_dir_path=$(readlink -f "$3")
+podman run -it --rm -v "$kube_config_path:/root/.kube/config:Z" -v "$work_dir_path:/config:Z" -v "$gpg_dir_path:/root/.gnupg:Z" helm-box:devel
